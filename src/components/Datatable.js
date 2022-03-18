013f5755734cd05a22db076e0e7f0cc053dd092d
Kwame <gtwene001@st.ug.edu.gh>
2022-03-18 13:22:34 +0000

    updated

--- src/components/Datatable.js
+++ src/components/Datatable.js
@@ -1,13 +1,68 @@
 import React from "react";
 import MUIDataTable from "mui-datatables";
 
-const columns = ["Name", "Company", "City", "State"];
+const columns = [
+  "Account Number",
+  "Account Description",
+  "Customer ID",
+  "Product Description",
+  "Arm Officer",
+  "Date Opened",
+  "Status",
+  "Created By",
+];
 
 const data = [
-  ["Joe James", "Test Corp", "Yonkers", "NY"],
-  ["John Walsh", "Test Corp", "Hartford", "CT"],
-  ["Bob Herm", "Test Corp", "Tampa", "FL"],
-  ["James Houston", "Test Corp", "Dallas", "TX"],
+  [
+    "10001",
+    "Savings Account",
+    "112",
+    "NY",
+    "ArmOfficer",
+    "25 May 2000",
+    "Active",
+    "Godfrey",
+  ],
+  [
+    "10002",
+    "Current Account",
+    "113",
+    "CT",
+    "ArmOfficer",
+    "25 May 2001",
+    "Inactive",
+    "Godfrey",
+  ],
+  [
+    "10003",
+    "Current Account",
+    "114",
+    "FL",
+    "ArmOfficer",
+    "25 May 2002",
+    "Active",
+    "Godfrey",
+  ],
+  [
+    "10004",
+    "Savings Account",
+    "116",
+    "TX",
+    "ArmOfficer",
+    "25 May 2003",
+    "Inactive",
+    "Godfrey",
+  ],
+  [
+    "10005",
+    "Savings Account",
+    "116",
+    "TX",
+    "ArmOfficer",
+    "25 May 2003",
+    "Inactive",
+    "Godfrey",
+  ],
 ];
 
 const options = {
@@ -18,7 +73,7 @@ const Datatable = () => {
   return (
     <div>
       <MUIDataTable
-        title={"Employee List"}
+        title={"X-100 Datatable"}
         data={data}
         columns={columns}
         options={options}
