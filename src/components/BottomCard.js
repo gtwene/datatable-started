05e093c38059057f6c05d7e81903c242245657ef
Kwame <gtwene001@st.ug.edu.gh>
2022-03-18 21:57:43 +0000

    update_two

--- src/components/BottomCard.js
+++ src/components/BottomCard.js
@@ -7,6 +7,17 @@ import Button from "@mui/material/Button";
 import Fade from "@mui/material/Fade";
 import Paper from "@mui/material/Paper";
 import { Card } from "@mui/material";
+import Divider from "@mui/material/Divider";
+
+import MenuList from "@mui/material/MenuList";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+
+import ContentCut from "@mui/icons-material/ContentCut";
+import ContentCopy from "@mui/icons-material/ContentCopy";
+import ContentPaste from "@mui/icons-material/ContentPaste";
+import Cloud from "@mui/icons-material/Cloud";
 
 export default function PositionedPopper() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -24,11 +35,8 @@ export default function PositionedPopper() {
       <Popper open={open} anchorEl={anchorEl} placement={placement} transition>
         {({ TransitionProps }) => (
           <Fade {...TransitionProps} timeout={350}>
-            <Card>
-              {/* <Typography sx={{ p: 2 }}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. 
-              </Typography> */}
+            {/* <Card>
+             
               <Box
                 className="p-5"
                 sx={{ border: 1, p: 1, bgcolor: "background.paper" }}
@@ -36,14 +44,52 @@ export default function PositionedPopper() {
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry.
               </Box>
-            </Card>
+            </Card> */}
+            <Paper sx={{ width: 320, maxWidth: "100%" }}>
+              <MenuList>
+                <MenuItem>
+                  <ListItemIcon>
+                    <ContentCut fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Cut</ListItemText>
+                  <Typography variant="body2" color="text.secondary">
+                    ⌘X
+                  </Typography>
+                </MenuItem>
+                <MenuItem>
+                  <ListItemIcon>
+                    <ContentCopy fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Copy</ListItemText>
+                  <Typography variant="body2" color="text.secondary">
+                    ⌘C
+                  </Typography>
+                </MenuItem>
+                <MenuItem>
+                  <ListItemIcon>
+                    <ContentPaste fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Paste</ListItemText>
+                  <Typography variant="body2" color="text.secondary">
+                    ⌘V
+                  </Typography>
+                </MenuItem>
+                <Divider />
+                <MenuItem>
+                  <ListItemIcon>
+                    <Cloud fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Web Clipboard</ListItemText>
+                </MenuItem>
+              </MenuList>
+            </Paper>
           </Fade>
         )}
       </Popper>
       <Grid container justifyContent="center">
-        <Grid className="absolute bottom-0 right-20 m-20 cursor-pointer">
+        <Grid className="absolute bottom-0 right-0 cursor-pointer">
           <Card
-            className="p-10 m-10 rounded border-2 shadow-xl shadow-black text-xl font-bold"
+            className="p-10  rounded border-2 shadow-xl shadow-black text-xl font-bold"
             onClick={handleClick("top")}
           >
             Knowledge Database
