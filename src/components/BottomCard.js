89b7d9892afd2b8dfb5c872ec68453913d15b1c2
gtwene <gtwene001@st.ug.edu.gh>
2022-03-21 11:37:38 +0000

    push_btn

--- src/components/BottomCard.js
+++ src/components/BottomCard.js
@@ -13,11 +13,19 @@ import MenuList from "@mui/material/MenuList";
 import MenuItem from "@mui/material/MenuItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemIcon from "@mui/material/ListItemIcon";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
+import LocalPoliceIcon from "@mui/icons-material/LocalPolice";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
+import SourceIcon from "@mui/icons-material/Source";
+import ProductionQuantityLimitsIcon from "@mui/icons-material/ProductionQuantityLimits";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 import ContentCut from "@mui/icons-material/ContentCut";
 import ContentCopy from "@mui/icons-material/ContentCopy";
 import ContentPaste from "@mui/icons-material/ContentPaste";
 import Cloud from "@mui/icons-material/Cloud";
+import "../App.css";
 
 export default function PositionedPopper() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -34,7 +42,7 @@ export default function PositionedPopper() {
     <Box sx={{ width: 500 }}>
       <Popper open={open} anchorEl={anchorEl} placement={placement} transition>
         {({ TransitionProps }) => (
-          <Fade {...TransitionProps} timeout={350}>
+          <Fade {...TransitionProps} timeout={2000}>
             {/* <Card>
              
               <Box
@@ -45,54 +53,99 @@ export default function PositionedPopper() {
                 industry.
               </Box>
             </Card> */}
-            <Paper sx={{ width: 320, maxWidth: "100%" }}>
+            <Paper sx={{ width: 250, maxWidth: "100%" }}>
               <MenuList>
                 <MenuItem>
                   <ListItemIcon>
-                    <ContentCut fontSize="small" />
+                    <QuestionMarkIcon fontSize="small" />
                   </ListItemIcon>
-                  <ListItemText>Cut</ListItemText>
-                  <Typography variant="body2" color="text.secondary">
+                  <ListItemText>General Enquiry</ListItemText>
+                  {/* <Typography variant="body2" color="text.secondary">
                     ⌘X
-                  </Typography>
+                  </Typography> */}
                 </MenuItem>
+                <Divider />
                 <MenuItem>
                   <ListItemIcon>
-                    <ContentCopy fontSize="small" />
+                    <GroupAddIcon fontSize="small" />
                   </ListItemIcon>
-                  <ListItemText>Copy</ListItemText>
-                  <Typography variant="body2" color="text.secondary">
+                  <ListItemText>Creating Account</ListItemText>
+                  {/* <Typography variant="body2" color="text.secondary">
                     ⌘C
-                  </Typography>
+                  </Typography> */}
                 </MenuItem>
+                <Divider />
                 <MenuItem>
                   <ListItemIcon>
                     <ContentPaste fontSize="small" />
                   </ListItemIcon>
-                  <ListItemText>Paste</ListItemText>
-                  <Typography variant="body2" color="text.secondary">
+                  <ListItemText>Help Desk</ListItemText>
+                  {/* <Typography variant="body2" color="text.secondary">
                     ⌘V
-                  </Typography>
+                  </Typography> */}
                 </MenuItem>
                 <Divider />
                 <MenuItem>
                   <ListItemIcon>
                     <Cloud fontSize="small" />
                   </ListItemIcon>
-                  <ListItemText>Web Clipboard</ListItemText>
+                  <ListItemText>X-100 Enterprise</ListItemText>
+                </MenuItem>
+                <Divider />
+                <MenuItem>
+                  <ListItemIcon>
+                    <LocalPoliceIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>About Arm Officer</ListItemText>
+                </MenuItem>
+                <Divider />
+                <MenuItem>
+                  <ListItemIcon>
+                    <SupportAgentIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Customer Care</ListItemText>
+                </MenuItem>
+                <Divider />
+                <MenuItem>
+                  <ListItemIcon>
+                    <SourceIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Source Branch</ListItemText>
+                </MenuItem>
+                <Divider />
+                <MenuItem>
+                  <ListItemIcon>
+                    <ProductionQuantityLimitsIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Product Description</ListItemText>
+                </MenuItem>
+                <Divider />
+                <MenuItem>
+                  <ListItemIcon>
+                    <MoreHorizIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Status</ListItemText>
+                </MenuItem>
+                <Divider />
+                <MenuItem>
+                  <ListItemIcon>
+                    <Cloud fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Account Number</ListItemText>
                 </MenuItem>
               </MenuList>
             </Paper>
           </Fade>
         )}
       </Popper>
-      <Grid container justifyContent="center">
-        <Grid className="absolute bottom-0 right-0 cursor-pointer">
+      <Grid  container justifyContent="center">
+        <Grid className="absolute bottom-0 right-0 cursor-pointer " sx={{ width: 250, maxWidth: "100%" }}>
           <Card
-            className="p-10  rounded border-2 shadow-xl shadow-black text-xl font-bold"
+            style={{ backgroundColor: "green", color: "white" }}
+            className="p-2 border-2 shadow-xl shadow-black font-bold"
             onClick={handleClick("top")}
           >
-            Knowledge Database
+           &nbsp; Knowledge Database &nbsp;
           </Card>
         </Grid>
       </Grid>
